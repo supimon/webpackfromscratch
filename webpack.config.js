ff2503d482e0df6b7702ec1aeb65ff065608399d
Supi <supimon@gmail.com>
2019-08-09 21:42:25 +0530

    file structure ready

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,6 @@ const pagesArr = require("./config/pages");
 
 const commonConfig = merge([
   {
-    entry: {
-      vendor: ["./src/vendor.js", "./src/vendor.scss"]
-    }
-  },
-  {
     module: {
       rules: [
         {
@@ -36,7 +31,6 @@ const productionConfig = merge([
 
 const developmentConfig = merge([
   parts.devServer({
-    // Customize host/port here if needed
     host: process.env.HOST,
     port: process.env.PORT
   }),
@@ -45,28 +39,6 @@ const developmentConfig = merge([
 
 module.exports = mode => {
   const pages = pagesArr;
-  // const pages = [
-  //   parts.page({
-  //     filename: "./component1page/component1page.html",
-  //     template: "./src/pages/component1page/component1page.pug",
-  //     entry: {
-  //       "./component1Page/component1Page": [
-  //         "./src/pages/component1page/component1page.js",
-  //         "./src/pages/component1page/component1page.scss"
-  //       ]
-  //     }
-  //   }),
-  //   parts.page({
-  //     filename: "./component2page/component2page.html",
-  //     template: "./src/pages/component2page/component2page.pug",
-  //     entry: {
-  //       "./component2Page/component2Page": [
-  //         "./src/pages/component2page/component2page.js",
-  //         "./src/pages/component2page/component2page.scss"
-  //       ]
-  //     }
-  //   })
-  // ];
   const config = mode === "production" ? productionConfig : developmentConfig;
 
   return pages.map(page => merge(commonConfig, config, page, { mode }));
